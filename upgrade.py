8b5649d76737a95bdcaf53c2fd33adc98e9c3a84
Clovis NZOUENDJOU <clovis@anybox.fr>
2018-05-18 12:54:09 +0200

    Add more repo and init upgrade script

--- upgrade.py
+++ upgrade.py
@@ -7,13 +7,38 @@ and to provide an example of how this works.
 
 
 def run(session, logger):
-    """Update all modules."""
+    """Update script"""
     if session.is_initialization:
-        logger.warn("Usage of upgrade script for initialization detected. "
-                    "You should consider customizing the present upgrade "
-                    "script to add modules install commands. The present "
-                    "script is at : %s (byte-compiled form)",
-                    __file__)
+        modules = [
+            'website_event_sale', 'account',
+            'theme_impacto', 'bi_website_support_ticket',
+            'project_start_stop', 'employee_document',
+            'print_project_report', 'project_kanban',
+            'backend_theme_v11', 'attachment_large_object',
+        ]
+        logger.info(u"Fresh database - Installing modules %r" % modules)
+        session.install_modules(modules)
         return
+
+    if session.db_version <= '1.0':
+        logger.info("ATTACHMENT LARGE OBJECT")
+        session.env['ir.config_parameter'].set_param(
+            'ir_attachment.location', 'postgresql:lobject')
+        install_module_if_not_already_done(
+            session, logger, ['attachment_large_object'])
+
     logger.info("Default upgrade procedure : updating all modules.")
     session.update_modules(['all'])
+
+
+def install_module_if_not_already_done(session, logger, modules):
+    logger.info("Installing modules %r if it is not already done" % modules)
+    install_modules = []
+    installed_modules = session.env['ir.module.module'].search([('state', '=', 'installed')])
+    installed_modules_names = [m.name for m in installed_modules]
+    for module in modules:
+        if module in installed_modules_names:
+            continue
+        install_modules.append(module)
+    if install_modules:
+        session.install_modules(install_modules)
